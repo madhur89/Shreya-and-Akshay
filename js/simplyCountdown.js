863be98ef116c09efde7f7fdf2fc6cf637c484ec
Madhur <68078506+madhur89@users.noreply.github.com>
2024-04-16 11:14:56 +0530

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -108,7 +108,7 @@
 
         // Set the target date to 45 days from now
         var currentDate = new Date();
-        currentDate.setDate(currentDate.getDate() + 12);
+        currentDate.setDate(currentDate.getDate() + 10);
 
         var interval,
             targetDate,
